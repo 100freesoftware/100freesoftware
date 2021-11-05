20cfa84220564b61dfde6c645d682f29f40edb9b
100% Free Software <93046703+100freesoftware@users.noreply.github.com>
2021-11-05 09:41:53 +0100

    Create arch-linux-automatic-system-update.sh
    draft versie

---
+++ arch-linux-automatic-system-update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Description:  automatic system update
+#
+# Usage:        system_update
+#
+# Example:      ./system_update
+#
+# Author:       100% Free Software
+#
+# Last update: 5 november 2021
+#
+# NOT ADVISED TO USE
+
+[ $EUID -ne 0 ] && { echo "$0 - Foutmelding: dit script moet worden uitgevoerd als root of een gebruiker uit de wheel groep via sudo."; exit 1; }
+
+read -p "Attentie AJB: je systeem wordt geupdate en gereboot. Wijzigingen kunnen niet worden geannuleerd. Weet je zeker dat je verder wilt gaan (Y/n)? " q
+[[ $q != "Y" ]] && exit 1
+
+sudo pacman -Syy
+[ $(sudo pacman -Su --print | wc -c) == 0 ] && { echo "Geen updates beschikbaar"; exit 1; }
+
+pacman -Syy && pacman -Su --noconfirm && reboot
+
+exit 0
